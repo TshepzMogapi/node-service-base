210f5b0c3dea494ab68bbf1b878c351f5908c977
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2021-08-23 19:28:17 +0200

    use email ejs template

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -1,15 +1,25 @@
+const path = require('path');
+const ejs = require('ejs');
 const emailService = require('../services/email.service');
 
 const sendEmail = async (req, res) => {
   try {
     const { email } = req.body;
-    res.send({ msg: 'testing' });
+    const userEmail = email;
+
+    const emailTemplate = await ejs
+      .renderFile(path.join(__dirname, '../views/testing.ejs'), {
+        confirm_link: `http://localhost:3006/auth?email=${userEmail}`
+      });
+
     const mailOptions = {
       from: 'no-reply@domain.com',
       to: email,
       subject: 'Testing Testing',
-      html: '<strong>testing</strong>',
+      html: emailTemplate,
     };
+    res.send({ msg: 'testing' });
+
     emailService.sendEmail(mailOptions);
   } catch (error) {
     console.log(error);
